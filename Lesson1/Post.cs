39d86a4fb40c2bd2658db96fd9730e86572d44c8
Konstantin Grimashevich <k.grimashevich@gmail.com>
2022-09-21 23:19:20 +0300

    Lesson1

---
+++ Lesson1/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+	internal class Post
+	{
+		public int userId { get; set; }
+		public int id { get; set; }
+		public string? title { get; set; }
+		public string? body { get; set; }
+
+		public override string ToString()
+		{
+			return $"{userId}\n{id}\n{title}\n{body}";
+		}
+	}
+}
---
+++ Lesson1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace Lesson1
+{
+	internal class Program
+	{
+		static readonly HttpClient client = new HttpClient();
+
+		static void Main()
+		{
+			List<Task<Post>> taskList = new List<Task<Post>>();
+			
+			
+			for (int i = 4; i < 14; i++)
+			{
+				taskList.Add(GetPostAsync(i));
+			}
+			Task.WhenAll(taskList).Wait();
+			Task.WaitAll(WriteToFile(taskList));
+		}
+
+		static async Task<Post?> GetPostAsync(int id)
+		{
+			Post? post;
+			
+			try
+			{
+				Console.WriteLine($"Task id {id} start...");
+				string responseBody = await client.GetStringAsync($"https://jsonplaceholder.typicode.com/posts/{id}");
+				post = JsonSerializer.Deserialize<Post>(responseBody);
+			}
+			catch (HttpRequestException e)
+			{
+				Console.WriteLine("\nException Caught!");
+				Console.WriteLine("Message :{0} ", e.Message);
+				post = null;
+			}
+			Console.WriteLine($"Task id {id} end...");
+			return post;
+		}
+
+		static async Task WriteToFile(List<Task<Post>> taskList)
+		{
+			StringBuilder result = new StringBuilder();
+
+			foreach (var task in taskList)
+				result.Append(task.Result);
+			await File.WriteAllTextAsync("result.txt", result.ToString());
+		}
+	}
+}
